230f207867cab287374d6de90bd67d597b2fc383
devwenderson <wend.devdesigner@gmail.com>
2024-08-16 09:14:18 -0300

    cria users wrapper

--- apis/cli_cadastro_clientes/main.py
+++ apis/cli_cadastro_clientes/main.py
@@ -1,4 +1,8 @@
 from users_wrapper import Users_wrapper
 
 users = Users_wrapper()
-print(users.list_users())
+# print(users.create_user("Arnaldo", "Fonsa", "arnaldo@email"))
+print(users.delete_user(2))
+# print(users.list_users())
+# print(users.detail_user(4))
+# print(users.update_user(2, "Alguém"))
--- apis/cli_cadastro_clientes/users_wrapper.py
+++ apis/cli_cadastro_clientes/users_wrapper.py
@@ -1,9 +1,72 @@
 import requests
 
 class Users_wrapper:
-    api_url = "https://jsonplaceholder.typicode.com/users"
-    
+    api_url = "https://jsonplaceholder.typicode.com/users/"
+
     def list_users(self):
-        response = requests.get(self.api_url).json()
-        return response
+        response = requests.get(self.api_url)
+        return response.json()
+
+    def create_user(self, name, username, email):
+        user = {
+            "id": 1,
+            "name": name,
+            "username": username,
+            "email": email,
+            "address": {
+                "street": None,
+                "suite": None,
+                "city": None,
+                "zipcode": None,
+                "geo": {
+                    "lat": None,
+                    "lng": None
+                }
+            },
+            "phone": None,
+            "website": None,
+            "company": {
+                "name": None,
+                "catchPhrase": None,
+                "bs": None
+            }
+        }
+        response = requests.post(self.api_url, json=user)
+        return response.status_code
+
+    def delete_user(self, id):
+        response = requests.delete(self.api_url + f"{id}")
+        return response.status_code
     
+    def detail_user(self, id):
+        response = requests.get(self.api_url + f"{id}")
+        return response.json()
+    
+    def update_user(self, id, name=None, username=None, email=None):
+        response = requests.get(self.api_url + f"{id}")
+        user = {
+            "id": None,
+            "name": name,
+            "username": username,
+            "email": email,
+            "address": {
+                "street": None,
+                "suite": None,
+                "city": None,
+                "zipcode": None,
+                "geo": {
+                    "lat": None,
+                    "lng": None
+                }
+            },
+            "phone": None,
+            "website": None,
+            "company": {
+                "name": None,
+                "catchPhrase": None,
+                "bs": None
+            }
+        }
+        response = requests.put(response, json=user)
+        return response.status_code
+
