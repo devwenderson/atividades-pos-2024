24c9a7ca493f1d7a9d6e657efe0a92b6f22c232e
devwenderson <wend.devdesigner@gmail.com>
2024-08-30 22:31:33 -0300

    atividade de autenticação

---
+++ auth/github_api/github.py
@@ -0,0 +1,40 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+password = "SUA CHAVE DE ACESSO"
+
+available_funcs = {
+    'follow': requests.put,
+    'unfollow': requests.delete,
+}
+
+# Lista as funções
+print("Escolha o número da função desejada")
+print("="*30)
+for i, function_name in enumerate(available_funcs.keys(), start=1):
+    print(f"{i} - {function_name}")
+print("="*30)
+
+# Escolher a função
+choice = int(input("Sua escolha: "))
+
+# Caso seja uma opção errada
+while (choice < 1 or choice > len(available_funcs.keys())):
+    print("Escolha inválida")
+    print("="*30)
+    choice = int(input("Sua escolha: "))
+
+
+chosen_function_name = list(available_funcs.keys())[choice - 1] # pega o nome da função
+chosen_function = available_funcs[chosen_function_name] # seleciona a função
+
+if choice == 1:
+    user = input("Digite o nome do usuário para seguir: ")
+    response = chosen_function(f'https://api.github.com/user/following/{user}',
+            auth = HTTPBasicAuth('SEU USUARIO', password))
+if choice == 2:
+    user = input("Digite o nome do usuário para deixar de seguir: ")
+    response = chosen_function(f'https://api.github.com/user/following/{user}',
+            auth = HTTPBasicAuth('SEU USUARIO', password))
+
+print(response.status_code)
---
+++ auth/suap_api/suap.py
@@ -0,0 +1,35 @@
+import requests, os, json
+from getpass import getpass
+
+api_url = "https://suap.ifrn.edu.br/api/"
+
+def auth(api_url):
+    user = input("Matrícula: ")
+    password = getpass("Senha: ")
+
+    data = {"username":user,"password":password}
+
+    response = requests.post(api_url+"v2/autenticacao/token/", json=data)
+    return response.json()["access"]
+
+def boletim_request(api_url, token, ano_letivo):
+    headers = {
+        "Authorization": f'Bearer {token}'
+    }
+    response = requests.get(f"{api_url}v2/minhas-informacoes/boletim/{ano_letivo}/1", headers=headers)
+    return response.json()
+
+if (os.path.isfile("auth/suap_api/token.json")):
+    with open("auth/suap_api/token.json") as file:
+        token = json.load(file)['token']
+        response = boletim_request(api_url, token, 2023)
+        for disciplina in response:
+            print(f"{disciplina['disciplina']} - Média final: {disciplina['media_disciplina']}")  
+else:
+    with open("auth/suap_api/token.json", "w") as file:
+        token = auth(api_url)
+        data = {"token": token}
+        json.dump(data, file)
+
+
+
