4628e51dce499a70d79eab8c4a8bb8aaafc5dc79
devwenderson <wend.devdesigner@gmail.com>
2024-10-17 08:56:24 -0300

    wip

--- clients/api_fetch/script.js
+++ clients/api_fetch/script.js
@@ -1,13 +1,21 @@
-var pokemon_atual = 1
+const listaPokemons = document.getElementById("lista-pokemons");
 async function fetchPokemonsData(id_pokemon) {
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id_pokemon}`);
         // Tratamento de erro
-        if (!response.ok) {
-            throw new Error("Erro: " + response.status )
-        }
+        handleRequestError(response);
         const data = await response.json();
         // console.log(data);
+
+        const pokemonDiv = document.createElement("div");
+        pokemonDiv.classList.add("pokemon");
+        pokemonDiv.innerHTML = `
+            <div class="pokemon-infos">
+                <h4>${data.name}</h4>
+            </div>
+        `;
+        listaPokemons.appendChild(pokemonDiv);
+
     } catch (error) {
         console.log("Erro: ", error)
     }
@@ -17,27 +25,32 @@ async function fetchTypesData() {
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/type/`);
         // Tratamento de erro
-        if (!response.ok) {
-            throw new Error("Erro: " + response.status )
-        }
+        handleRequestError(response);
         const data = await response.json();
 
         // Cria filtro de busca por tipo de pokemon
-        let searchByTypeElement = document.getElementById("search-by-type");
+        let nav = document.getElementById("nav");
         data.results.forEach(type => {
-            let searchOption = document.createElement("option");
-            searchOption.innerHTML = `
-                <option value="${type.name}">${type.name}</option>
-            `;
-            searchByTypeElement.appendChild(searchOption);
+            let filterButton = document.createElement("button");
+            filterButton.classList.add(`${type.name}`, 'button')
+            filterButton.textContent = `${type.name}`
+            nav.appendChild(filterButton);
         });
-        console.log(data.results);
     } catch (error) {
         console.log("Erro: ", error)
     }
-}
+};
+
+function handleRequestError (response) {
+    if (!response.ok) {
+        throw new Error("Erro: " + response.status )
+    }
+};
 
 window.onload = () => {
     fetchTypesData();
-}
+    for (let i = 1; i <= 120; i++) {
+        fetchPokemonsData(i);
+    }
+};
 
