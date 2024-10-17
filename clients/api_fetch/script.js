a5db9256ba971b8fd5fda9ca3686e1c5c9e9beb6
devwenderson <wend.devdesigner@gmail.com>
2024-10-17 11:39:28 -0300

    PokeApi fetch

--- clients/api_fetch/script.js
+++ clients/api_fetch/script.js
@@ -5,13 +5,55 @@ async function fetchPokemonsData(id_pokemon) {
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${id_pokemon}`
     );
+
     // Tratamento de erro
     handleRequestError(response);
     const data = await response.json();
-    return data;
+
+    return data; // Retorna os dados do Pokémon
+
   } catch (error) {
     console.log("Erro: ", error);
   }
+}
+
+async function displayPokemon(pokemonData) {
+  // Melhora o visual do id do pokemon
+  let pokeId = pokemonData.id.toString();
+  if (pokeId.length === 1) {
+    pokeId = "00" + pokeId;
+  } else if (pokeId.length === 2) {
+    pokeId = "0" + pokeId;
+  }
+
+  // Cria lista com os tipos do pokemon
+  let pokeTypes = pokemonData.types
+    .map((type) => `<p class="${type.type.name} type">${type.type.name}</p>`)
+    .join("");
+
+  const pokemonDiv = document.createElement("div");
+  pokemonDiv.classList.add("pokemon");
+  pokemonDiv.innerHTML = `
+            <div class="pokemon-infos">
+                <div class="pokemon-img-container">
+                    <img src="${
+                      pokemonData["sprites"]["versions"]["generation-v"][
+                        "black-white"
+                      ]["animated"]["front_default"]
+                    }" class="pokemon-img">
+                </div>
+
+                <div class="pokemon-name-container">
+                    <p class="pokemon-id">#${pokeId}</p>
+                    <h4 class="pokemon-name">${capitalize(pokemonData.name)}</h4>
+                </div>
+
+                <div class="pokemon-types">
+                    ${pokeTypes}
+                </div>
+            </div>
+        `;
+  listaPokemons.appendChild(pokemonDiv);
 }
 
 async function fetchTypesData() {
@@ -27,9 +69,9 @@ async function fetchTypesData() {
     let filterButton = document.createElement("button");
     filterButton.classList.add("button");
     filterButton.textContent = "Todos";
-    filterButtonId = "todos-pokemons";
+    const filterButtonId = "todos-pokemons";
     filterButton.id = filterButtonId;
-    filterButton.onclick = () => filterByType(filterButtonId);
+    filterButton.onclick = () => filterByType("all");
     nav.appendChild(filterButton);
 
     // Cria filtro de busca por tipo de pokemon
@@ -37,70 +79,33 @@ async function fetchTypesData() {
       let filterButton = document.createElement("button");
       filterButton.classList.add(`${type.name}`, "button");
       filterButton.textContent = `${type.name}`;
-      filterButtonId = `${type.name}`;
+      const filterButtonId = `${type.name}`;
       filterButton.id = filterButtonId;
-      filterButton.onclick = () => filterByType(filterButtonId);
+      filterButton.onclick = () => filterByType(type.name);
       nav.appendChild(filterButton);
     });
+
   } catch (error) {
     console.log("Erro: ", error);
   }
 }
 
-function exibirPokemons(data) {
-  // Melhora o visual do id do pokemon
-  let pokeId = data.id.toString();
-  if (pokeId.length === 1) {
-    pokeId = "00" + pokeId;
-  } else if (pokeId.length === 2) {
-    pokeId = "0" + pokeId;
-  }
+async function filterByType(type) {
+  listaPokemons.innerHTML = "";
 
-  // Cria lista com os tipos do pokemon
-  let pokeTypes = data.types
-    .map((type) => `<p class="${type.type.name} type">${type.type.name}</p>`)
-    .join("");
-
-  const pokemonDiv = document.createElement("div");
-  pokemonDiv.classList.add("pokemon");
-  pokemonDiv.innerHTML = `
-    <div class="pokemon-infos">
-        <div class="pokemon-img-container">
-            <img src="${
-              data["sprites"]["versions"]["generation-v"]["black-white"][
-                "animated"
-              ]["front_default"]
-            }" class="pokemon-img">
-        </div>
-
-        <div class="pokemon-name-container">
-            <p class="pokemon-id">#${pokeId}</p>
-            <h4 class="pokemon-name">${capitalize(data.name)}</h4>
-        </div>
-
-        <div class="pokemon-types">
-            ${pokeTypes}
-        </div>
-    </div>
-    `;
-  listaPokemons.appendChild(pokemonDiv);
-}
-
-async function filterByType(buttonId) {
-  if (buttonId == "todos-pokemons") {
-    alert("oi")
+  if (type === "all") {
+    // Carrega todos os Pokémons
     for (let i = 1; i <= 120; i++) {
-        const data = await fetchPokemonsData(i);
-        console.log(data)
-        exibirPokemons(data);
+      const data = await fetchPokemonsData(i);
+      displayPokemon(data);
     }
   } else {
     // Filtra por tipo
     for (let i = 1; i <= 120; i++) {
       const data = await fetchPokemonsData(i);
-      const types = data.types.map((type) => type.type.name);
-      if (types.some((type) => type.includes(buttonId))) {
-        exibirPokemons(data);
+      const isTypeMatch = data.types.some(t => t.type.name === type);
+      if (isTypeMatch) {
+        displayPokemon(data);
       }
     }
   }
@@ -120,6 +125,6 @@ function capitalize(str) {
 window.onload = () => {
   fetchTypesData();
   for (let i = 1; i <= 120; i++) {
-    fetchPokemonsData(i).then((data) => exibirPokemons(data));
+    fetchPokemonsData(i).then(data => displayPokemon(data));
   }
 };
