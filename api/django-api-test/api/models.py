6c41b2c89db6c53774997a7d53a83a16806361cf
devwenderson <wend.devdesigner@gmail.com>
2024-11-27 16:14:38 -0300

    adiciona função str nas models

--- api/django-api-test/api/models.py
+++ api/django-api-test/api/models.py
@@ -4,12 +4,21 @@ class Artista(models.Model):
     nome = models.CharField(max_length=100)
     local = models.CharField(max_length=100)
 
+    def __str__(self):  
+        return f'{self.nome}'
+
 class Album(models.Model):
     artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     ano = models.IntegerField()
 
+    def __str__(self):  
+        return f'{self.nome} - {self.artista} - {self.ano}'
+
 class Musica(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     segundos = models.IntegerField()
+
+    def __str__(self):  
+        return f'{self.nome} {self.album} - {self.segundos}'
